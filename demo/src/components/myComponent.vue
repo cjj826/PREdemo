d60ca146cdf524c7b3db8eeb1b9eca1a0676d24e
陈俊杰 <chenjunjie@chenjunjiedeMacBook-Pro.local>
2024-03-05 15:02:16 +0800

    cjj: 增加上传文件功能

--- demo/src/components/myComponent.vue
+++ demo/src/components/myComponent.vue
@@ -8,6 +8,12 @@
         {{ option.text }}
       </option>
     </select>
+    <div class="file-upload-wrapper">
+      <button @click="triggerFileInput" class="file-upload-button">选择文件</button>
+      <span class="file-name">{{ fileName }}</span>
+      <input type="file" @change="loadJsonFile" accept=".json" ref="fileInput" class="file-input" />
+    </div>
+
     <button @click="submit" class="button">导出为JSON</button>
   </div>
 </template>
@@ -24,6 +30,14 @@ const options = ref([
   { value: '选项3', text: '选项3' },
 ]);
 
+const fileInput = ref(null);
+const fileName = ref("未选择文件");
+
+const triggerFileInput = () => {
+  fileInput.value.click(); // 触发input的点击事件
+};
+
+// 输入有效性验证
 const validateFloat = (event) => {
   // 使用正则表达式验证输入是否为浮点数，允许负数和小数点
   if (!event.target.value.match(/^[-]?[0-9]*\.?[0-9]*$/)) {
@@ -35,6 +49,38 @@ const validateFloat = (event) => {
   }
 };
 
+
+// 从JSON文件加载数据
+const loadJsonFile = (event) => {
+  const file = event.target.files[0];
+  fileName.value = file ? file.name : "未选择文件";
+  
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      try {
+        const content = e.target.result;
+        const jsonData = JSON.parse(content);
+        alert("open success")
+        // 用读取的数据来更新前端的数据
+        inputValue.value = jsonData["key1"]
+        selectedOption.value = jsonData["key2"]
+        strValue.value = jsonData["key3"]
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+        alert("文件内容不是有效的JSON格式！");
+      }
+    };
+    reader.onerror = (error) => {
+      console.error("File reading error:", error);
+      alert("读取文件时发生错误，请重试。");
+    };
+    console.log("here")
+    reader.readAsText(file); // 以文本形式读取文件
+  }
+};
+
+// 提交选择并导出json文件
 const submit = () => {
   const data = {
     key1: inputValue.value,
@@ -95,6 +141,45 @@ const submit = () => {
 .button:hover {
   background-color: #2563eb; /* 鼠标悬停时改变按钮背景色，增加交互性 */
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* 鼠标悬停时增加阴影，增加立体感 */
+}
+
+.file-upload-wrapper {
+  display: flex;
+  justify-content: space-between; /* 使按钮和文件名分布在两端 */
+  align-items: center;
+  width: 100%; /* 容器宽度为100% */
+}
+
+.file-upload-button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+.file-upload-button:hover {
+  background-color: #0056b3;
+}
+
+.file-input {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  opacity: 0;
+  overflow: hidden;
+  clip: rect(1px, 1px, 1px, 1px);
+}
+
+.file-name {
+  flex-grow: 1; /* 文件名占据剩余空间 */
+  padding: 0 10px; /* 保持文字不贴边 */
+  text-align: left;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis; /* 长文件名用省略号表示 */
 }
 
 </style>
